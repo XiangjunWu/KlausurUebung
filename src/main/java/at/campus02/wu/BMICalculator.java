4390b3c5ee16e516231266d636e4e10fc9181e89
Xiangjun.Wu <s51043@edu.campus02.at>
2021-11-23 22:11:53 +0100

    create BMICalculator

---
+++ src/main/java/at/campus02/wu/BMICalculator.java
@@ -0,0 +1,46 @@
+package at.campus02.wu;
+
+public class BMICalculator {
+
+    public String firstname;
+    public String lastname;
+    public int bodyHeight;
+    public double bodyWeight;
+    public char gender;
+
+    public BMICalculator(String firstname, String lastname, int bodyHeight, double bodyWeight, char gender) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.bodyHeight = bodyHeight;
+        this.bodyWeight = bodyWeight;
+        this.gender = gender;
+    }
+
+    public void setBodyHeight(int bodyHeight) {
+        this.bodyHeight = bodyHeight;
+    }
+
+    public void setBodyWeight(double bodyWeight) {
+        this.bodyWeight = bodyWeight;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public int getBodyHeight() {
+        return bodyHeight;
+    }
+
+    public double getBodyWeight() {
+        return bodyWeight;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+}
